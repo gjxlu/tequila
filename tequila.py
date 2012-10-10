5ed4ecfb406781d17ea41aab9e856401e32c7206
ZUO Haocheng <zuohaocheng1022@gmail.com>
2012-10-10 20:15:48 +0800

    email reading

--- tequila.py
+++ tequila.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
-print 'hello world'
+import getpass, imaplib
+
+M = imaplib.IMAP4_SSL('imap.gmail.com')
+M.login(getpass.getuser(), getpass.getpass())
+#M.login('zuohaocheng1022', getpass.getpass())
+M.select()
+typ, data = M.search(None, 'ALL')
+for num in data[0].split():
+    typ, data = M.fetch(num, '(RFC822)')
+    print 'Message %s\n%s\n' % (num, data[0][1])
+M.close()
+M.logout()
